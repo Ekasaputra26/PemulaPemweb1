66d4adc7d236f1b5671ec0df92cc043354013077
Ekasaputra26 <muhammadekasaputra@saintek.unipdu.ac.id>
2024-11-01 22:29:20 -0400

    update

--- Pertemuan 5/tes.js
+++ Pertemuan 5/tes.js
@@ -6,3 +6,26 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+const navLinks = document.querySelectorAll('.nav');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+// Smooth scroll effect
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        // Scroll to the target section with smooth behavior
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
